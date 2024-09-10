adb8cb2d76714fe78f1d5ab03bccd1691347d289
adarsh62656 <adarsh62656@gmail.com>
2024-09-10 21:53:38 +0530

    added submodule

--- main.tf
+++ main.tf
@@ -6,12 +6,6 @@ output "random_integer" {
   value = random_integer.priority.result
 }
 terraform {
-  backend "s3" {
-    bucket         = "adarsh-backend-jd"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
 }
 
 output "dockerfile_content" {
